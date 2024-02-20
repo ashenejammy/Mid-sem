d40610f1c9ae1f704b5f107f41f9ccf28d7468b9
Jonathan Ashene <ashenejonathan@gmail.com>
2024-02-20 12:01:57 +0300

    name.js

---
+++ name.js
@@ -0,0 +1,5 @@
+// Prompt the user for their name
+var name = prompt("What's your name?");
+
+// Greet the user
+alert("Hello, " + name + "! Welcome to our website!");
---
+++ school-name.js
@@ -0,0 +1,8 @@
+// Prompt the user for their school
+var school = prompt("What school do you attend?");
+
+// Prompt the user for their age
+var age = prompt("How old are you?");
+
+// Display the information
+alert("You attend " + school + " and you are " + age + " years old.");
